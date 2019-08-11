dc26ef9bdd1ba979c812d87e1ca948ea76048e33
Cory Thomas <corywythomas@gmail.com>
2019-08-11 21:41:45 +0100

    Update web.config to point at new db instance on localmachine.
    Fix minor typo in Calculate.cshtml.
    Split direct calls to context and controller logic, into separate layers, repo and service.
    Repo for data exclusively, Service for any logic sitting on top of that.
    Pre DI changes to the BonusPoolController.

---
+++ Solution/SynetecMvcAssessment/Repositories/BonusPoolRepository.cs
@@ -0,0 +1,40 @@
+﻿using InterviewTestTemplatev2.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace InterviewTestTemplatev2.Repositories
+{
+    public class BonusPoolRepository
+    {
+        #region Fields
+        private MvcInterviewV3Entities1 _bonusPoolContext;
+        #endregion
+
+
+        #region Constructors
+        public BonusPoolRepository(MvcInterviewV3Entities1 bonusPoolContext)
+        {
+            _bonusPoolContext = bonusPoolContext;
+        }
+
+        public List<HrEmployee> GetEmployeeList()
+        {
+            return _bonusPoolContext.HrEmployees.ToList<HrEmployee>();
+        }
+
+        public int GetEmployeeTotalSalary()
+        {
+            return (int)_bonusPoolContext.HrEmployees.Sum(item => item.Salary);
+        }
+
+        public HrEmployee GetEmployeeByID(int selectedEmployeeId)
+        {
+            return (HrEmployee)_bonusPoolContext.HrEmployees.FirstOrDefault(item => item.ID == selectedEmployeeId);
+        }
+        #endregion
+
+
+    }
+}
---
+++ Solution/SynetecMvcAssessment/Services/BonusPoolService.cs
@@ -0,0 +1,63 @@
+﻿using InterviewTestTemplatev2.Data;
+using InterviewTestTemplatev2.Models;
+using InterviewTestTemplatev2.Repositories;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace InterviewTestTemplatev2.Services
+{
+    public class BonusPoolService
+    {
+        #region Fields
+        private BonusPoolRepository _bonusPoolRepository;
+        #endregion
+
+        #region Constructors
+        public BonusPoolService(BonusPoolRepository bonusPoolRepository)
+        {
+            _bonusPoolRepository = bonusPoolRepository;
+        }
+        #endregion
+
+        public List<HrEmployee> GetEmployeeList()
+        {
+            return _bonusPoolRepository.GetEmployeeList();
+        }
+
+        public int GetEmployeeTotalSalary()
+        {
+            return _bonusPoolRepository.GetEmployeeTotalSalary();
+        }
+
+        public HrEmployee GetEmployeeByID(int selectedEmployeeId)
+        {
+            return _bonusPoolRepository.GetEmployeeByID(selectedEmployeeId);
+        }
+
+        public BonusPoolCalculatorResultModel CalculateEmployeesBonusShare(BonusPoolCalculatorModel model)
+        {
+            int selectedEmployeeId = model.SelectedEmployeeId;
+            int totalBonusPool = model.BonusPoolAmount;
+
+            //load the details of the selected employee using the ID
+            HrEmployee hrEmployee = this.GetEmployeeByID(selectedEmployeeId);
+
+            int employeeSalary = hrEmployee.Salary;
+
+            //get the total salary budget for the company
+            int totalSalary = this.GetEmployeeTotalSalary();
+
+            //calculate the bonus allocation for the employee
+            decimal bonusPercentage = (decimal)employeeSalary / (decimal)totalSalary;
+            int bonusAllocation = (int)(bonusPercentage * totalBonusPool);
+
+            BonusPoolCalculatorResultModel result = new BonusPoolCalculatorResultModel();
+            result.hrEmployee = hrEmployee;
+            result.bonusPoolAllocation = bonusAllocation;
+
+            return result;
+        }
+    }
+}
